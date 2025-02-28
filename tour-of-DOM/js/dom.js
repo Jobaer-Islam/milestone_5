3c6402c931c75341df282a1638be59fa6510d6ae
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-28 18:48:47 +0600

    DOM

--- tour-of-DOM/js/dom.js
+++ tour-of-DOM/js/dom.js
@@ -1,1 +1,16 @@
-console.log("hello from js");
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    // console.log(section);
+    section.style.border = '2px solid red'; 
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '8px';
+    section.style.paddingLeft = '5px';
+    section.style.backgroundColor = 'lightgray';
+}
+
+// const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'yellow';
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('text-center');
+placesContainer.classList.remove('large-text');
